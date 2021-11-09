fd8161f3a8a503ab94c122dda25468970422501e
mmraihan <mm_raihan@live.com>
2021-11-09 15:05:40 +0600

    Insert Data

--- CW-Practise/Day-7-Insert-Update/Program.cs
+++ CW-Practise/Day-7-Insert-Update/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Day_7_Insert_Update.Database;
+using Day_7_Insert_Update.Models.EntityModels;
+using System;
 
 namespace Day_7_Insert_Update
 {
@@ -6,7 +8,33 @@ namespace Day_7_Insert_Update
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Category category = new Category()
+            {
+                Name ="Furnitures"
+
+            };
+            Category category2 = new Category()
+            {
+                Name = "Electronics"
+            };
+            Category category3 = new Category()
+            {
+                Name = "Clothes"
+            };
+
+            SMECommerceDbContext db = new SMECommerceDbContext();
+
+            db.Categories.Add(category);
+            db.Categories.Add(category2); //Tracking all command
+            db.Categories.Add(category3); //Tracking all command
+
+            int successCount= db.SaveChanges(); //Finalyy hited in database
+
+            if (successCount>0)
+            {
+                Console.WriteLine($"Data saved successfully. Success Count: {successCount}");
+            }
+            Console.ReadKey();
         }
     }
 }

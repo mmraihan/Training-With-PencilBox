8e20662ac2d07d343665d5e71ca04422a0864ba7
mmraihan <mm_raihan@live.com>
2021-11-24 02:55:13 +0600

    Form Tag Helper

---
+++ RazorSyntaxExamples/ViewBagDemo/Models/FormEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewBagDemo.Models
+{
+    public class FormEmployee
+    {
+        public string Name { get; set; }
+
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set; }
+        public double Salary { get; set; }
+        public int Married { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,Female
+    }
+}

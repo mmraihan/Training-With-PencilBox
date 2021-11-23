1cad88f1a5a03e766dbbbbe943418d512f5088cf
mmraihan <mm_raihan@live.com>
2021-11-24 00:07:30 +0600

    ViewBag

--- RazorSyntaxExamples/ViewBagDemo/Controllers/ProductController.cs
+++ RazorSyntaxExamples/ViewBagDemo/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ViewBagDemo.Models;
 
 namespace ViewBagDemo.Controllers
 {
@@ -10,13 +11,26 @@ namespace ViewBagDemo.Controllers
     {
         public IActionResult Index()
         {
+            #region ViewData
             ViewData["name"] = "I am from ViewData";
             ViewData["names"] = new List<string>()
             {
                 "Mahin","Raihan","Sahik"
             };
+            #endregion
 
 
+            #region BiewData from model
+
+            Product product = new Product()
+            {
+                Id = "101",
+                Name = "Laptop",
+                Category = "Electronics"
+            };
+               ViewData["prpductName"] = product;
+
+            #endregion
 
 
             return View();
---
+++ RazorSyntaxExamples/ViewBagDemo/Controllers/ViewBagController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewBagDemo.Models;
+
+namespace ViewBagDemo.Controllers
+{
+    public class ViewBagController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Hello I am from ViewBag";
+
+            Product productOfViewbag = new Product()
+            {
+                Id = "KeyBoard-101",
+                Name = "Keyboard",
+                Category="Electronics"
+            };
+
+            ViewBag.p = productOfViewbag;
+
+            return View();
+        }
+    }
+}

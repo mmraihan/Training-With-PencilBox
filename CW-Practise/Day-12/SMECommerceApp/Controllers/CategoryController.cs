ccf0ce8817b005bdcf2bcb0e01300030ec969589
mmraihan <mm_raihan@live.com>
2021-11-28 17:53:45 +0600

    Day-12-Template added

---
+++ CW-Practise/Day-12/SMECommerceApp/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMECommerce.Databases.DbContexts;
+using SMECommerce.Models.EntityModels;
+using SMECommerce.Repositories;
+using SMECommerceApp.Models.CategoryModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMECommerceApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to Category Controller";
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreate categoryCreate)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            if (categoryCreate.Name!=null)
+            {
+                var category = new Category()
+                {
+                    Name = categoryCreate.Name,
+                    Code=categoryCreate.Code,
+                    Description = categoryCreate.Description,
+
+                };
+                var isAdded = categoryRepository.Add(category);
+                if (isAdded)
+                {
+                    return View("Success");
+                }
+            }
+            return View();
+        }
+  
+  
+    }
+}
---
+++ CW-Practise/Day-12/SMECommerceApp/Models/CategoryModel/CategoryCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMECommerceApp.Models.CategoryModel
+{
+    public class CategoryCreate
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

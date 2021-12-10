d4251b70cd1d062e81c8ce0dcd179c8a2f7a8892
mmraihan <mm_raihan@live.com>
2021-12-10 16:11:20 +0600

    Day-13-DI Folder Organized

---
+++ CW-Practise/Day-13-DIP-DI/SMECommerce.Databases/DbContexts/SMECommerceDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SMECommerce.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMECommerce.Databases.DbContexts
+{
+    public class SMECommerceDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Products { get; set; }   
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=DESKTOP-4BMDG8B;Database=SMEDb;Trusted_Connection=True;MultipleActiveResultSets=True";
+            optionsBuilder
+                //.UseLazyLoadingProxies()
+                .UseSqlServer(connectionString);
+                
+        }
+    }
+}

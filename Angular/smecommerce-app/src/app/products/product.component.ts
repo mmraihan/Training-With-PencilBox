1d08edb5d89ad12d201d0990d4fa29c65c6dedea
mmraihan <mm_raihan@live.com>
2022-02-22 00:12:00 +0600

    Day 21: ngFor

--- Angular/smecommerce-app/src/app/products/product.component.ts
+++ Angular/smecommerce-app/src/app/products/product.component.ts
@@ -11,12 +11,26 @@ import {  Component } from "@angular/core";
 
 export class ProductComponent{
     showPrpduct: boolean= false;
- product={
+ products=[
+     {
      name: "Sony TV",
      price: 67999,
      code:"S-11",
      imageUrl: "https://www.sony-asia.com/image/60fc808004d6860e433b5a4cafeb60d2?fmt=pjpeg&resMode=bisharp&wid=354"
- }
+    },
+    {
+        name: "Samsung TV",
+        price: 67999,
+        code:"S-11",
+        imageUrl: "https://www.sony-asia.com/image/60fc808004d6860e433b5a4cafeb60d2?fmt=pjpeg&resMode=bisharp&wid=354"
+    },
+    {
+        name: "LG TV",
+        price: 67999,
+        code:"S-11",
+        imageUrl: "https://www.sony-asia.com/image/60fc808004d6860e433b5a4cafeb60d2?fmt=pjpeg&resMode=bisharp&wid=354"
+       },
+]
 
  toggleProduct(){
     this.showPrpduct=!this.showPrpduct;

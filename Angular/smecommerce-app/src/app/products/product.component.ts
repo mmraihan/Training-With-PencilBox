b978fd3ca738be46ef6a3995a3928ff8baad993f
mmraihan <mm_raihan@live.com>
2022-02-21 23:39:02 +0600

    Day-22-Event Binding

--- Angular/smecommerce-app/src/app/products/product.component.ts
+++ Angular/smecommerce-app/src/app/products/product.component.ts
@@ -14,6 +14,7 @@ export class ProductComponent{
      name: "Sony TV",
      price: 67999,
      code:"S-11",
+     imageUrl: "https://www.sony-asia.com/image/60fc808004d6860e433b5a4cafeb60d2?fmt=pjpeg&resMode=bisharp&wid=354"
  }
 
 }
--- Angular/smecommerce-app/src/app/products/product.module.ts
+++ Angular/smecommerce-app/src/app/products/product.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { ProductComponent } from "./product.component";
 
 
 
 @NgModule({
-    declarations: [ ProductComponent],  
+    declarations: [ ProductComponent],
+    imports: [FormsModule],
     exports:[ProductComponent]
 
 })
 
 export class ProductModule{
-
+    
 }

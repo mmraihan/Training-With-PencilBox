113fd849a3d015284a51a274b7091ce829497db3
mmraihan <mm_raihan@live.com>
2022-02-22 18:07:11 +0600

    Day 22: Custom property @Input().

---
+++ Angular/smecommerce-app/src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,15 @@
+import {  Component, Input } from "@angular/core";
+
+
+@Component(
+    {
+        selector: 'product-detail',
+        templateUrl: './product-detail.component.html',
+        styleUrls:['./product-detail.component.css']
+    }
+)
+
+export class ProductDetailComponent{
+   @Input() product: any;
+
+}
--- Angular/smecommerce-app/src/app/products/product.module.ts
+++ Angular/smecommerce-app/src/app/products/product.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { ProductComponent } from "./product.component";
 
 
 
 @NgModule({
-    declarations: [ ProductComponent],
+    declarations: [ ProductComponent,ProductDetailComponent],
     imports: [FormsModule,
               CommonModule],
     exports:[ProductComponent]
